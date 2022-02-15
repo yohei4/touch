3c2c69578ea9582e42f9b5944ea3b52648e5049a
yohei4 <yohei000421@gmail.com>
2022-02-16 07:59:37 +0900

    ファイルアップローダー2回開かれるのバグ修正

--- laravel/resources/ts/components/utility/form/Select.tsx
+++ laravel/resources/ts/components/utility/form/Select.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 interface SelectProps {
     id: string;
     name: string;
-    txt: string;
+    label: string;
     options: string[];
     value?: string;
     required?: boolean;
     onChange(event: any): void;
 };
 
-const Select = React.memo((props: SelectProps) => {
+const Select = forwardRef((props: SelectProps, ref: React.Ref<HTMLSelectElement>) => {
 
     return (
         <div className="form-item">
             <div className="form-item__label">
-                <label className="form-item__label--txt" htmlFor={props.name}>{props.txt}</label>
+                <label className="form-item__label--txt" htmlFor={props.name}>{props.label}</label>
                 { props.required === true ? <span className="form-required"></span> : null }
             </div>
             <div className="form-item__container">
