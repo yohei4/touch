ae2cbf9648dccfd2f7bdfc37cfa8141c1d061cc8
yohei4 <yohei000421@gmail.com>
2022-02-17 08:17:07 +0900

    更新

--- laravel/resources/ts/components/utility/loader/Loader.tsx
+++ laravel/resources/ts/components/utility/loader/Loader.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-
 const Loader = () => {
     return (
-        <div className='loader'>
-            <div className='ball-scale-multiple'>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
+        <div className='ball-scale-multiple'>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 };
